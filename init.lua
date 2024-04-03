a4ac1b90a6ab03ea87e2d071f2e52df965313090
ThePython10110 <87204246+ThePython10110@users.noreply.github.com>
2024-04-02 18:04:49 -0700

    Ensure that benefits apply to capital lag

--- init.lua
+++ init.lua
@@ -10,7 +10,7 @@ end)
 
 
 minetest.register_on_chat_message(function(name, message)
-	if string.find(message, "l+a+g+") then
+	if string.find(message:lower(), "l+a+g+") then
 		minetest.chat_send_player(name, minetest.colorize("red", "[ADMIN] No, the server isn't lagging"))
 		local player = minetest.get_player_by_name(name)
 		if player then
